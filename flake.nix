ac13a3aefc4e8499195168690376d9579c4979a4
Yipeng Sun <syp@umd.edu>
2021-07-12 03:20:29 +0200

    Use ROOT 6.24 stack by default.

--- flake.nix
+++ flake.nix
@@ -27,11 +27,11 @@
 
             # Pick your favorite version of ROOT here
             # ROOT 6.24 stack
-            #root_6_24_02
+            root_6_24_02
 
             # ROOT 6.16 stack
-            root_6_16_00
-            freetype # Weird UI library dependency required by macOS
+            #root_6_16_00
+            #freetype # Weird UI library dependency required by macOS
 
             # ROOT 5.34 stack
             #root_5_34_38
