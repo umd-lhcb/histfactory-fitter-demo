b8de2233e5f02bc3fe2830f2f3b628a69db29c05
Yipeng Sun <syp@umd.edu>
2022-01-04 00:08:20 +0100

    WIP #3: Remove unused plotting functions.

--- include/plot.h
+++ include/plot.h
@@ -1,6 +1,6 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Mon Jan 03, 2022 at 03:23 PM +0100
+// Last Change: Mon Jan 03, 2022 at 11:18 PM +0100
 
 #ifndef _FIT_DEMO_PLOT_H_
 #define _FIT_DEMO_PLOT_H_
@@ -88,88 +88,6 @@ std::unique_ptr<TCanvas> plotFitVars(std::vector<RooPlot*>& frames,
     setFitVarsFrameStyle(cvs.get(), frames[idx], idx + 1);
     frames[idx]->Draw();
     lbl->DrawLatex(anchors[idx], frames[idx]->GetMaximum() * 0.95, "Demo");
-  }
-
-  return cvs;
-}
-
-//////////////////////////////////////////
-// Plot binned fit variables with pulls //
-//////////////////////////////////////////
-
-TPad* setBinnedFitVarPad(char const* plot_name, double xlow = 0.,
-                         double ylow = 0., double xup = 1., double yup = 0.3,
-                         float tMargin = 0., float bMargin = 0.5,
-                         bool gridy = true) {
-  auto pad = new TPad(plot_name, plot_name, xlow, ylow, xup, yup);
-
-  pad->SetFillColor(0);
-  pad->SetFillStyle(0);
-  pad->SetTickx();
-  pad->SetTicky();
-  if (gridy) pad->SetGridy();
-
-  pad->SetLeftMargin(pad->GetLeftMargin() + 0.08);
-  pad->SetTopMargin(tMargin);
-  pad->SetRightMargin(0.04);
-  pad->SetBottomMargin(bMargin);
-
-  return pad;
-}
-
-void setBinnedFitVarPullFrameStyle(RooPlot* frame) {
-  frame->SetTitle("");
-
-  frame->GetXaxis()->SetTitleSize(0.36 * 0.22 / 0.3);
-  frame->GetXaxis()->SetTitleOffset(0.78);
-  frame->GetXaxis()->SetTickLength(0.10);
-  frame->GetXaxis()->SetLabelSize(0.33 * 0.22 / 0.3);
-
-  frame->GetYaxis()->SetTitle("Pulls");
-  frame->GetYaxis()->SetTitleSize(0.33 * 0.22 / 0.3);
-  frame->GetYaxis()->SetTitleOffset(0.2);
-  frame->GetYaxis()->SetTickLength(0.05);
-  frame->GetYaxis()->SetLabelSize(0.33 * 0.22 / 0.3);
-  frame->GetYaxis()->SetLabelOffset(99);
-  frame->GetYaxis()->SetNdivisions(205);
-}
-
-void setBinnedFitVarMainFrameStyle(RooPlot* frame, char const* lbl) {
-  frame->SetTitle(lbl);
-  frame->SetTitleFont(132, "t");
-
-  frame->GetXaxis()->SetTitleSize(0.09 * 0.78 / 0.7);
-  frame->GetXaxis()->SetTitleOffset(0.95);
-  frame->GetXaxis()->SetLabelSize(0.09 * 0.78 / 0.7);
-
-  frame->GetYaxis()->SetTitleSize(0.09 * 0.78 / 0.7);
-  frame->GetYaxis()->SetTitleOffset(0.95);
-  frame->GetYaxis()->SetLabelSize(0.09 * 0.78 / 0.7);
-  frame->GetYaxis()->SetNdivisions(506);
-}
-
-std::unique_ptr<TCanvas> plotBinnedFitVarsWithPulls(
-    std::vector<RooPlot*>& frames, std::vector<RooPlot*>& pulls,
-    std::vector<char const*>& cuts, std::vector<char const*>& lbls,
-    char const* name, int width, int height) {
-  auto cvs  = std::make_unique<TCanvas>(name, name, width, height);
-  auto lbl  = makeLabel();
-  auto bins = cuts.size();
-  char plotName[32];
-
-  cvs->Divide(bins, 2);
-
-  for (auto idx = 0; idx < frames.size(); idx++) {
-    // Bottom pulls
-    cvs->cd(idx + 1);
-    sprintf(plotName, "bottompad_%d", idx);
-
-    auto padBot = setBinnedFitVarPad(plotName);
-    padBot->Draw();
-    padBot->cd();
-
-    // Prepare for bottom pull histogram
-    auto histPull = pulls[idx];
   }
 
   return cvs;
